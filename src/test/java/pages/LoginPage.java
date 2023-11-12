8db2fe28cf7cd192910a68f6b9d11583bed69f4b
Aziz <utr1994@mail.ru>
2023-11-12 00:57:25 -0800

    Add files via upload

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import steps.BaseClass;
+
+public class LoginPage extends BaseClass {
+
+    @FindBy(id="Email")
+    public WebElement email;
+    @FindBy(id="Password")
+    public WebElement password;
+    @FindBy(id="RememberMe")
+    public WebElement rememberMe;
+
+
+    public LoginPage(WebDriver driver) {
+        driver = BaseClass.driver;
+        PageFactory.initElements(driver, this);
+    }
+}
