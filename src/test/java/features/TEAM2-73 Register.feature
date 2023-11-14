5d6813e87d70cab918902e65cdf9f5abc8a3a52a
vianat <vianatdev@gmail.com>
2023-11-14 11:24:13 -0800

    register

---
+++ src/test/java/features/TEAM2-73 Register.feature
@@ -0,0 +1,39 @@
+@web
+Feature: TEAM2-3 Add and Delete a New Customer
+  As a administrator I want to:
+    add a new customer,
+    and delete the customer from system.
+
+  @web
+  Scenario Outline: <browser>
+    Given I navigate to "https://env-sun.portnov.com/"
+    And wait 200
+    When Click Login button
+    And Enter email "team-sun@noreply.portnov.com"
+    And Enter password "ow!<lSy~2H87"
+    And Push Enter
+    And wait 200
+    Then Make sure you see Log out button and text: "Welcome to our store"
+    And I navigate to "https://env-sun.portnov.com/Admin"
+    And wait 200
+    And Expand customers panel
+    And Click on customers in panel
+    And wait 200
+    And Click Add new button
+    And wait 200
+    And Type email in [email] field
+    And Click [save] button
+    And Verify that the client has been created and exists in the client list
+    And Click [edit] button for this customer
+    And wait 200
+    And Click [delete] button
+    And Accept alert
+    And wait 200
+    Then Verify that the client has been deleted
+
+    Examples:
+      | browser |
+      | firefox |
+      | chrome  |
+      | edge    |
+      #| safari  |
---
+++ src/test/java/steps/RegisterTest.java
@@ -0,0 +1,64 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.RegisterPage;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
+public class LoginTest extends BaseClass {
+
+    MainPage mp = new MainPage(driver);
+    LoginPage lp = new LoginPage(driver);
+
+    @Given("I navigate to {string}")
+    public void i_navigate_to(String url) {
+        driver.get(url);
+    }
+
+    @When("Click Login button")
+    public LoginPage click_login_button() {
+        mp.loginBtn.click();
+        return new LoginPage(driver);
+    }
+
+    @And("Enter email {string}")
+    public void enterEmail(String email) {
+        lp.email.sendKeys(email);
+    }
+
+    @And("Enter password {string}")
+    public void enterPassword(String pass) {
+        lp.password.sendKeys(pass);
+    }
+
+    @And("Push Enter")
+    public MainPage pushEnter() {
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.ENTER).perform();
+        return new MainPage(driver);
+    }
+
+    @Then("Make sure you see Log out button and text: {string}")
+    public void makeSureYouSeeLogOutButtonAndText(String text) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        String actual = mp.logOutBtn.getText();
+        String expected = "Log out";
+        assertEquals(actual, expected);
+        assertEquals(mp.welcome.getText(), text);
+    }
+
+    @And("Click [Register] button")
+    public RegisterPage clickRegisterButton() {
+        mp.registerBtn.click();
+        return new RegisterPage(driver);
+    }
+}
